3df7b8a6d4afd42ca8249b556d8d3402078346ab
Hao Ding <ding.hao@hotmail.com>
2022-04-03 22:31:09 +1000

    made the first command-line wrapper for simulate.py

---
+++ run_simulate.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+###########################################################################
+###-- NOTICE: RUN UNDER pmparesults directories !!!
+############################################################################
+import os, sys
+import argparse
+from sterne import simulate
+
+code_description = "A command-line wrapper for sterne.simulate.simulate(). NOTE to self (others can ignore): Run under pmparesults directories!"
+example = "run_simulate.py 57849 J0030+0451.inits J0030+0451.pmpar.in '' -r J0030+0451.pmpar.in.preliminary -s [[0],[0],[-1],[0],[0],[-1],[0],[0]] -i 5000 -n 30"
+
+parser = argparse.ArgumentParser(description=code_description, prog='run_simulate.py', usage=example)
+parser.add_argument("epoch", metavar="epoch", type=int,
+                    help="reference epoch in MJD")
+parser.add_argument("priors", metavar="priors", type=str,
+                    help="the .inits file")
+parser.add_argument("pmparins", metavar="pmparins", type=str, 
+                    nargs='+', help='.pmpar.in and parfiles files')
+parser.add_argument("-r", "--prelimpmpars", dest="prelimpmpars", 
+                    metavar="prelimpmpars", nargs='+', type=str, 
+                    help=".pmpar.in.preliminary files")
+parser.add_argument("-s", "--shares", dest="shares", type=str, 
+                    metavar="shares", help="a 2-D array assigning which parameters to share (see sterne.simulate.simulate)")
+parser.add_argument("-i", "--iterations", dest="iterations", type=int, 
+                    default=1000, metavar="iterations", help="the depths that random walkers cover")
+parser.add_argument("-n", "--nwalkers", dest="nwalkers", type=int, default=30,
+                    metavar="nwalkers", help="the number of random walkers")
+parser.add_argument("-a", "--a1dot", dest="a1dot_constraints", type=str, 
+                    default=False, metavar="a1dot_constraints", 
+                    help="a1dot constraints, a list of list of 2 floats. e.g. [[mu, sigma], []], (both in lt-sec/sec), where mu and sigma refers to the Gaussian distribution for a1dot. The length of a1dot_constraint needs to match len(pmparins), unless None.")
+
+options         = parser.parse_args()
+refepoch        = options.epoch
+initsfiles      = options.priors
+pmparins        = options.pmparins
+
+kwargs = {}
+kwargs['iterations'] = options.iterations
+kwargs['nwalkers'] = options.nwalkers
+kwargs['pmparin_preliminaries'] = options.prelimpmpars
+exec("kwargs['shares'] = %s" % options.shares)
+exec("kwargs['a1dot_constraints'] = %s" % options.a1dot_constraints)
+
+
+print('\ndeleting old files in the outdir directory now.\n')
+os.system('rm -rf outdir/*')
+
+simulate.simulate(refepoch, initsfiles, *pmparins, **kwargs)
